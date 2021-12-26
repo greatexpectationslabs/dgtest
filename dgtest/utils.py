8e29b921dae20becb6acaaa8926e35ab52cf1278
Chetan Kini <chetan@superconductive.com>
2021-12-26 11:49:38 -0500

    feat: add utils for finding all source/test files

--- dgtest/utils.py
+++ dgtest/utils.py
@@ -1,33 +1,49 @@
+import glob
 import pathlib
-from typing import List, Tuple
+from typing import List, Optional, Tuple
 
 import git
 
 
-def get_changed_files(source: str, branch: str) -> Tuple[List[str], List[str]]:
+def get_changed_files(branch: str) -> Tuple[List[str], List[str]]:
     repo = git.Repo()
     diff = repo.git.diff(branch, name_only=True)
     files = [f.strip() for f in diff.split("\n")]
-    changed_source_files = _get_changed_source_files(files, source)
-    changed_test_files = _get_changed_test_files(files)
+    changed_source_files = _filter_source_files(files)
+    changed_test_files = _filter_test_files(files)
     return changed_source_files, changed_test_files
 
 
-def _get_changed_source_files(files: List[str], source: str) -> List[str]:
+def retrieve_all_source_files(source: str) -> List[str]:
+    all_files = _retrieve_all_py_files(source)
+    source_files = _filter_source_files(all_files)
+    return source_files
+
+
+def retrieve_all_test_files(source: str, tests: Optional[str] = None) -> List[str]:
+    all_files = _retrieve_all_py_files(source)
+    if tests:
+        all_files += _retrieve_all_py_files(tests)
+
+    test_files = _filter_test_files(all_files)
+    return test_files
+
+
+def _retrieve_all_py_files(directory: str) -> List[str]:
+    return [file for file in glob.glob(f"{directory}/**/*.py", recursive=True)]
+
+
+def _filter_source_files(files: List[str]) -> List[str]:
     source_files = []
     for file in files:
         path = pathlib.Path(file)
-        if (
-            not _is_existing_py_file(path)
-            or path.stem.startswith("test_")
-            or not str(path.parent).startswith(source)
-        ):
+        if not _is_existing_py_file(path) or path.stem.startswith("test_"):
             continue
         source_files.append(str(path))
     return source_files
 
 
-def _get_changed_test_files(files: List[str]) -> List[str]:
+def _filter_test_files(files: List[str]) -> List[str]:
     test_files = []
     for file in files:
         path = pathlib.Path(file)
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,71 +1,69 @@
+from typing import Callable, List, Tuple
 from unittest import mock
 
 import py
+import pytest
 
-from dgtest.utils import get_changed_files
+from dgtest.utils import (
+    get_changed_files,
+    retrieve_all_source_files,
+    retrieve_all_test_files,
+)
 
 
-def test_get_changed_files_only_source_files(tmpdir: py.path.local) -> None:
-    my_dir = tmpdir.mkdir("my_dir")
-    files = []
-    for name in ("foo.py", "bar.py", "baz.py"):
-        temp_file = my_dir.join(name)
-        temp_file.write("")
-        files.append(temp_file.strpath)
+@pytest.fixture(scope="function")
+def create_mock_codebase(tmpdir: py.path.local) -> Callable:
+    def _create_mock_codebase(directory: str, *files: str) -> Tuple[str, List[str]]:
+        my_dir = tmpdir.mkdir(directory)
+        my_files = []
+        for name in files:
+            temp_file = my_dir.join(name)
+            temp_file.write("")
+            my_files.append(temp_file.strpath)
+        return my_dir.strpath, my_files
 
+    return _create_mock_codebase
+
+
+def test_get_changed_files_only_source_files(create_mock_codebase: Callable) -> None:
+    _, files = create_mock_codebase("my_dir", "foo.py", "bar.py", "baz.py")
     with mock.patch("dgtest.utils.git.Repo") as mock_repo:
         mock_repo().git.diff.return_value = "\n".join(file for file in files)
-        changed_source_files, changed_test_files = get_changed_files(
-            my_dir.strpath, "origin/master"
-        )
+        changed_source_files, changed_test_files = get_changed_files("origin/master")
 
     assert len(changed_source_files) == 3
     assert len(changed_test_files) == 0
 
 
-def test_get_changed_files_both_source_and_test_files(tmpdir: py.path.local) -> None:
-    my_dir = tmpdir.mkdir("my_dir")
-    files = []
-    for name in (
+def test_get_changed_files_both_source_and_test_files(
+    create_mock_codebase: Callable,
+) -> None:
+    _, files = create_mock_codebase(
+        "my_dir",
         "foo.py",
         "bar.py",
         "baz.py",
         "test_foo.py",
         "test_bar.py",
         "test_baz.py",
-    ):
-        temp_file = my_dir.join(name)
-        temp_file.write("")
-        files.append(temp_file.strpath)
-
+    )
     with mock.patch("dgtest.utils.git.Repo") as mock_repo:
         mock_repo().git.diff.return_value = "\n".join(file for file in files)
-        changed_source_files, changed_test_files = get_changed_files(
-            my_dir.strpath, "origin/master"
-        )
+        changed_source_files, changed_test_files = get_changed_files("origin/master")
 
     assert len(changed_source_files) == 3
     assert len(changed_test_files) == 3
 
 
-def test_get_changed_files_excludes_non_py_files(tmpdir: py.path.local) -> None:
-    my_dir = tmpdir.mkdir("my_dir")
-    files = []
-    for name in (
-        "foo.yml",
-        "bar.json",
-        "README.md",
-        "LICENSE",
-    ):
-        temp_file = my_dir.join(name)
-        temp_file.write("")
-        files.append(temp_file.strpath)
-
+def test_get_changed_files_excludes_non_py_files(
+    create_mock_codebase: Callable,
+) -> None:
+    _, files = create_mock_codebase(
+        "my_dir", "foo.yml", "bar.json", "README.md", "LICENSE"
+    )
     with mock.patch("dgtest.utils.git.Repo") as mock_repo:
         mock_repo().git.diff.return_value = "\n".join(file for file in files)
-        changed_source_files, changed_test_files = get_changed_files(
-            my_dir.strpath, "origin/master"
-        )
+        changed_source_files, changed_test_files = get_changed_files("origin/master")
 
     assert len(changed_source_files) == 0
     assert len(changed_test_files) == 0
@@ -77,9 +75,35 @@ def test_get_changed_files_excludes_nonexistant_files(tmpdir: py.path.local) -> 
 
     with mock.patch("dgtest.utils.git.Repo") as mock_repo:
         mock_repo().git.diff.return_value = fake_file.strpath
-        changed_source_files, changed_test_files = get_changed_files(
-            my_dir.strpath, "origin/master"
-        )
+        changed_source_files, changed_test_files = get_changed_files("origin/master")
 
     assert len(changed_source_files) == 0
     assert len(changed_test_files) == 0
+
+
+def test_retrieve_all_source_files(create_mock_codebase: Callable) -> None:
+    my_dir, _ = create_mock_codebase(
+        "my_dir", "foo.py", "bar.py", "test_foo.py", "test_bar.py"
+    )
+    source_files = retrieve_all_source_files(my_dir)
+    assert len(source_files) == 2
+
+
+def test_retrieve_all_test_files_without_test_arg(
+    create_mock_codebase: Callable,
+) -> None:
+    my_source_dir, _ = create_mock_codebase(
+        "my_dir", "foo.py", "bar.py", "test_foo.py", "test_bar.py"
+    )
+    my_test_dir = None
+    test_files = retrieve_all_test_files(my_source_dir, my_test_dir)
+    assert len(test_files) == 2
+
+
+def test_retrieve_all_test_files_with_test_arg(create_mock_codebase: Callable) -> None:
+    my_source_dir, _ = create_mock_codebase(
+        "my_dir", "foo.py", "bar.py", "test_foo.py", "test_bar.py"
+    )
+    my_test_dir, _ = create_mock_codebase("tests", "test_baz.py", "test_qux.py")
+    test_files = retrieve_all_test_files(my_source_dir, my_test_dir)
+    assert len(test_files) == 4
