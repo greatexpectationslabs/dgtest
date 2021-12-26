ae252e29436795353e761bade30b2e43dc90175a
Chetan Kini <chetan@superconductive.com>
2021-12-26 13:50:07 -0500

    feat: add 'parse_import_nodes' func

--- dgtest/parse.py
+++ dgtest/parse.py
@@ -1,17 +1,20 @@
 import ast
-from collections import defaultdict
-from typing import DefaultDict, Dict, List, Set
+import difflib
+from collections import defaultdict, namedtuple
+from typing import DefaultDict, Dict, List, Optional, Set
+
+Import = namedtuple("Import", ["source", "module", "name", "alias"])
 
 
-def parse_definition_nodes(files: List[str]) -> DefaultDict[str, Set[str]]:
-    definition_map: DefaultDict[str, Set[str]] = defaultdict(set)
+def parse_definition_nodes_from_codebase(files: List[str]) -> Dict[str, Set[str]]:
+    definition_map: Dict[str, Set[str]] = {}
     for file in files:
-        file_definition_map = _parse_definition_nodes(file)
+        file_definition_map = parse_definition_nodes_from_file(file)
         update_dict(definition_map, file_definition_map)
     return definition_map
 
 
-def _parse_definition_nodes(file: str) -> DefaultDict[str, Set[str]]:
+def parse_definition_nodes_from_file(file: str) -> DefaultDict[str, Set[str]]:
     with open(file) as f:
         root = ast.parse(f.read(), file)
 
@@ -25,6 +28,69 @@ def _parse_definition_nodes(file: str) -> DefaultDict[str, Set[str]]:
         file_definition_map[name].add(file)
 
     return file_definition_map
+
+
+def parse_import_nodes_from_codebase(
+    files: List[str], source: str, definition_map: Dict[str, Set[str]]
+) -> Dict[str, Set[str]]:
+    imports: Dict[str, Set[str]] = {}
+    for file in files:
+        file_imports = parse_import_nodes_from_file(file, source, definition_map)
+        imports[file] = file_imports
+    return imports
+
+
+def parse_import_nodes_from_file(
+    file: str,
+    source: str,
+    definition_map: Dict[str, Set[str]],
+) -> Set[str]:
+    imports = _gather_import_nodes_from_file(file)
+    paths = set()
+    for import_ in imports:
+        path = _generate_path_from_import_node(import_, source, definition_map)
+        if path is not None:
+            paths.add(path)
+
+    return paths
+
+
+def _gather_import_nodes_from_file(file: str) -> List[Import]:
+    with open(file) as f:
+        root = ast.parse(f.read(), file)
+
+    imports = []
+    for node in root.body:
+        if isinstance(node, ast.Import):
+            module: List[str] = []
+        elif isinstance(node, ast.ImportFrom) and node.module is not None:
+            module = node.module.split(".")
+        else:
+            continue
+
+        for n in node.names:  # type: ignore [attr-defined] - mypy doesn't agree with Union[ast.Import, ast.ImportFrom] having a names attr
+            import_ = Import(file, module, n.name.split("."), n.asname)
+            imports.append(import_)
+
+    return imports
+
+
+def _generate_path_from_import_node(
+    import_: Import, source: str, definition_map: Dict[str, Set[str]]
+) -> Optional[str]:
+    partial_path = "/".join(m for m in import_.module)
+    if not import_.module or not import_.name or not partial_path.startswith(source):
+        return None
+    key = import_.name[0]
+    candidates: Set[str] = definition_map.get(key, set())
+    if len(candidates) == 0:
+        return None
+    elif len(candidates) == 1:
+        return list(candidates)[0]
+    else:
+        # If we're not sure of the origin, use the stringified import statement and make an educated guess
+        closest = difflib.get_close_matches(partial_path, candidates)[0]
+        return str(closest)
 
 
 def update_dict(A: Dict[str, Set[str]], B: Dict[str, Set[str]]) -> None:
--- tests/test_parse.py
+++ tests/test_parse.py
@@ -1,71 +1,73 @@
 import py
 
-from dgtest.parse import parse_definition_nodes, update_dict
+from dgtest.parse import (
+    parse_definition_nodes_from_codebase,
+    parse_definition_nodes_from_file,
+    parse_import_nodes_from_codebase,
+    parse_import_nodes_from_file,
+    update_dict,
+)
 
 
-def test_parse_definition_nodes_collects_function_definitions(
+def test_parse_definition_nodes_from_file_collects_function_definitions(
     tmpdir: py.path.local,
 ) -> None:
     my_dir = tmpdir.mkdir("my_dir")
     foo = my_dir.join("foo.py")
-    bar = my_dir.join("bar.py")
 
     foo.write(
         """
 def my_first_func():
     pass
-    """
-    )
-    bar.write(
-        """
-def my_second_func(a: int, b: int) -> int:
-    return a + b
+
+def my_second_func():
+    pass
     """
     )
 
-    definition_map = parse_definition_nodes([foo, bar])
+    file_definition_map = parse_definition_nodes_from_file(foo.strpath)
 
-    assert len(definition_map) == 2
+    assert len(file_definition_map) == 2
     assert (
-        isinstance(definition_map["my_first_func"], set)
-        and len(definition_map["my_first_func"]) == 1
+        isinstance(file_definition_map["my_first_func"], set)
+        and len(file_definition_map["my_first_func"]) == 1
     )
     assert (
-        isinstance(definition_map["my_second_func"], set)
-        and len(definition_map["my_second_func"]) == 1
+        isinstance(file_definition_map["my_second_func"], set)
+        and len(file_definition_map["my_second_func"]) == 1
     )
 
 
-def test_parse_definition_nodes_collects_class_definitions(
+def test_parse_definition_nodes_from_file_collects_class_definitions(
     tmpdir: py.path.local,
 ) -> None:
     my_dir = tmpdir.mkdir("my_dir")
     foo = my_dir.join("foo.py")
-    bar = my_dir.join("bar.py")
 
     foo.write(
-        """
-class Foo:
-    pass
-    """
-    )
-    bar.write(
         """
 class Bar:
+    pass
+
+class Baz:
     def __init__(self, a: int, b: int):
         self.a = a
         self.b = b
     """
     )
 
-    definition_map = parse_definition_nodes([foo, bar])
+    definition_map = parse_definition_nodes_from_file(foo.strpath)
 
     assert len(definition_map) == 2
-    assert isinstance(definition_map["Foo"], set) and len(definition_map["Bar"]) == 1
-    assert isinstance(definition_map["Bar"], set) and len(definition_map["Bar"]) == 1
+    assert (
+        isinstance(definition_map["Bar"], set) and foo.strpath in definition_map["Bar"]
+    )
+    assert (
+        isinstance(definition_map["Baz"], set) and foo.strpath in definition_map["Baz"]
+    )
 
 
-def test_parse_definition_nodes_accounts_for_namespace_collision(
+def test_parse_definition_nodes_from_codebase_accounts_for_namespace_collision(
     tmpdir: py.path.local,
 ) -> None:
     my_dir = tmpdir.mkdir("my_dir")
@@ -86,10 +88,135 @@ class Foo:
     """
     )
 
-    definition_map = parse_definition_nodes([foo, bar])
+    definition_map = parse_definition_nodes_from_codebase([foo.strpath, bar.strpath])
 
     assert len(definition_map) == 1
     assert isinstance(definition_map["Foo"], set) and len(definition_map["Foo"]) == 2
+
+
+def test_parse_import_nodes_from_file_with_no_relative_imports(
+    tmpdir: py.path.local,
+) -> None:
+    my_dir = tmpdir.mkdir("my_dir")
+    foo = my_dir.join("foo.py")
+    foo.write(
+        """
+import copy
+import datetime
+import json
+import logging
+import os
+from typing import Dict, List, Optional, Union
+from uuid import UUID
+    """
+    )
+
+    imports = parse_import_nodes_from_file(foo.strpath, "great_expectations", {})
+    assert imports == set()
+
+
+def test_parse_import_nodes_from_file_with_relative_imports(
+    tmpdir: py.path.local,
+) -> None:
+    my_dir = tmpdir.mkdir("my_dir")
+    foo = my_dir.join("foo.py")
+    foo.write(
+        """
+from my_package.util import my_first_func
+from my_package.core import MyClass
+from my_package.core.util import my_second_func, my_third_func
+    """
+    )
+
+    definition_map = {
+        "my_first_func": {"my_package/util/file.py"},
+        "MyClass": {"my_package/core/my_class.py"},
+        "my_second_func": {"my_package/core/util.py"},
+        "my_third_func": {"my_package/core/util.py"},
+    }
+
+    imports = parse_import_nodes_from_file(foo.strpath, "my_package", definition_map)
+    assert len(imports) == 3
+    assert all(
+        path in imports
+        for path in (
+            "my_package/util/file.py",
+            "my_package/core/my_class.py",
+            "my_package/core/util.py",
+        )
+    )
+
+
+def test_parse_import_nodes_from_file_with_ambiguous_imports(
+    tmpdir: py.path.local,
+) -> None:
+    my_dir = tmpdir.mkdir("my_dir")
+    foo = my_dir.join("foo.py")
+    foo.write(
+        """
+from my_package.util import my_first_func
+    """
+    )
+
+    # Namespace collision means we need to pick the closest import
+    definition_map = {
+        "my_first_func": {"my_package/util/file.py", "my_package/core/file.py"},
+    }
+
+    imports = parse_import_nodes_from_file(foo.strpath, "my_package", definition_map)
+    assert len(imports) == 1
+
+    # The import statement included "util" so we pick the first option
+    assert "my_package/util/file.py" in imports
+    assert "my_package/core/file.py" not in imports
+
+
+def test_parse_import_nodes_from_codebase(tmpdir: py.path.local) -> None:
+    my_dir = tmpdir.mkdir("my_dir")
+    foo = my_dir.join("foo.py")
+    bar = my_dir.join("bar.py")
+
+    foo.write(
+        """
+import copy
+import datetime
+import json
+import logging
+
+from my_package.core import MyClass
+from my_package.util import my_first_func
+    """
+    )
+    bar.write(
+        """
+import pandas as pd
+import numpy as np
+import os
+
+from my_package.core import MyClass
+from my_package.core.util import my_second_func, my_third_func
+    """
+    )
+
+    definition_map = {
+        "my_first_func": {"my_package/util/file.py"},
+        "MyClass": {"my_package/core/my_class.py"},
+        "my_second_func": {"my_package/core/util.py"},
+        "my_third_func": {"my_package/core/util.py"},
+    }
+
+    imports = parse_import_nodes_from_codebase(
+        [foo.strpath, bar.strpath], "my_package", definition_map
+    )
+    assert len(imports) == 2
+    assert imports[foo.strpath] == {
+        "my_package/util/file.py",
+        "my_package/core/my_class.py",
+    }
+    assert imports[bar.strpath] == {
+        "my_package/core/util.py",
+        "my_package/core/my_class.py",
+    }
 
 
 def test_update_dict_updates_existing_keys() -> None:
@@ -104,7 +231,7 @@ def test_update_dict_updates_existing_keys() -> None:
 
 def test_update_dict_adds_new_keys() -> None:
     A = {"foo": {"a", "b", "c"}}
-    B = {"foo": {"c", "d", "e"}}
+    B = {"bar": {"c", "d", "e"}}
 
     update_dict(A, B)
 
