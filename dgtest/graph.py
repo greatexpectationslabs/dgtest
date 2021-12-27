0d14878bd73af09ae7ff68ff36d8c5ba3f72b6e3
Chetan Kini <chetan@superconductive.com>
2021-12-27 09:00:10 -0500

    feat: finish graph functions and tests

--- dgtest/graph.py
+++ dgtest/graph.py
@@ -1,16 +1,24 @@
-from typing import Dict, Set
+from typing import Dict, List, Optional, Set
 
 
-def traverse_graph(
-    root: str, source: str, graph: Dict[str, Set[str]], depth: int
-) -> Set[str]:
+def determine_relevant_source_files(
+    source_dependency_graph: Dict[str, Set[str]], changed_files: List[str], depth: int
+) -> List[str]:
+    relevant_source_files: Set[str] = set()
+    for file in changed_files:
+        deps = _traverse_graph(file, source_dependency_graph, depth)
+        relevant_source_files.update(deps)
+    return sorted(relevant_source_files)
+
+
+def _traverse_graph(root: str, graph: Dict[str, Set[str]], depth: int) -> Set[str]:
     stack = [(root, depth)]
     seen: Set[str] = set()
 
     while stack:
         node, d = stack.pop()
         # If we've hit a cycle, traversed past our stated depth, or touched a file that isn't from our source, throw away the node
-        if node in seen or d <= 0 or not node.startswith(source):
+        if node in seen or d <= 0:
             continue
         seen.add(node)
         for child in graph.get(node, {}):
@@ -19,13 +27,30 @@ def traverse_graph(
     return seen
 
 
-def determine_relevant_source_files() -> None:
-    pass  # TODO(cdkini): Open to write!
+def determine_test_candidates(
+    tests_dependency_graph: Dict[str, Set[str]],
+    source_files: List[str],
+    changed_test_files: List[str],
+) -> List[str]:
+    # Ensure we include any test files that were identified in the git diff
+    candidates = {file for file in changed_test_files}
+
+    for file in source_files:
+        for test in tests_dependency_graph.get(file, []):
+            candidates.add(test)
+    return sorted(candidates)
 
 
-def determine_test_candidates() -> None:
-    pass  # TODO(cdkini): Open to write!
-
-
-def determine_tests_to_run() -> None:
-    pass  # TODO(cdkini): Open to write!
+def determine_tests_to_run(
+    test_candidates: List[str], ignore_paths: List[str], filter_: Optional[str] = None
+) -> List[str]:
+    files_to_test = []
+    for file in test_candidates:
+        # Throw out files that are in our ignore list
+        if any(file.startswith(path) for path in ignore_paths):
+            continue
+        # Throw out files that aren't explicitly part of a filter (if supplied)
+        if filter_ and not file.startswith(filter_):
+            continue
+        files_to_test.append(file)
+    return files_to_test
--- tests/unit/test_graph.py
+++ tests/unit/test_graph.py
@@ -1,15 +1,93 @@
-from dgtest.graph import traverse_graph
+from typing import Dict, Set
+
+from dgtest.graph import (
+    determine_relevant_source_files,
+    determine_test_candidates,
+    determine_tests_to_run,
+)
 
 
-def test_traverse_graph() -> None:
+def test_determine_relevant_source_file_to_run() -> None:
     graph = {
         "my_source/A": {"my_source/C"},
         "my_source/B": {"my_source/A", "my_source/D"},
-        "my_source/C": {"my_source/B"},
+        "my_source/C": {"my_source/B", "not_my_source/F"},
         "my_source/D": {"my_source/C"},
         "my_source/E": {"my_source/C", "my_source/A"},
     }
 
-    res = traverse_graph("my_source/A", "my_source", graph, depth=2)
+    res = determine_relevant_source_files(graph, ["my_source/A"], depth=2)
     assert len(res) == 2
     assert all(node in res for node in ("my_source/A", "my_source/C"))
+
+
+def test_determine_test_candidates_without_changed_test_files() -> None:
+    tests_dependency_graph = {
+        "source1": {"my_test1", "my_test2"},
+        "source2": {"my_test1"},
+        "source3": {"my_test1", "my_test3"},
+        "source4": {"my_test4", "my_test5", "my_test6"},
+        "source5": {"my_test1", "my_test3"},
+    }
+    source_files = ["source1", "source2", "source3"]
+
+    candidates = determine_test_candidates(tests_dependency_graph, source_files, [])
+    assert candidates == ["my_test1", "my_test2", "my_test3"]
+
+
+def test_determine_test_candidates_with_changed_test_files() -> None:
+    tests_dependency_graph = {
+        "source1": {"my_test1", "my_test2"},
+        "source2": {"my_test1"},
+        "source3": {"my_test1", "my_test3"},
+        "source4": {"my_test4", "my_test5", "my_test6"},
+        "source5": {"my_test1", "my_test3"},
+    }
+    source_files = ["source1", "source2", "source3"]
+    changed_test_files = ["my_test4", "my_test5"]
+
+    candidates = determine_test_candidates(
+        tests_dependency_graph, source_files, changed_test_files
+    )
+    assert candidates == ["my_test1", "my_test2", "my_test3", "my_test4", "my_test5"]
+
+
+def test_determine_tests_to_run_with_ignore_paths() -> None:
+    candidates = [
+        "a/b/c",
+        "a/b/d",
+        "a/c/e",
+        "a/c/f",
+        "b/d/e",
+    ]
+    ignore_paths = ["b", "a/c"]
+
+    tests = determine_tests_to_run(candidates, ignore_paths)
+    assert tests == ["a/b/c", "a/b/d"]
+
+
+def test_determine_tests_to_run_with_filter() -> None:
+    candidates = [
+        "a/b/c",
+        "a/b/d",
+        "a/c/e",
+        "a/c/f",
+        "b/d/e",
+    ]
+
+    tests = determine_tests_to_run(candidates, [], filter_="a/c")
+    assert tests == ["a/c/e", "a/c/f"]
+
+
+def test_determine_tests_to_run_with_ignore_paths_and_filter() -> None:
+    candidates = [
+        "a/b/c",
+        "a/b/d",
+        "a/c/e",
+        "a/c/f",
+        "b/d/e",
+    ]
+    ignore_paths = ["b", "a/c"]
+
+    tests = determine_tests_to_run(candidates, ignore_paths, filter_="a/b/c")
+    assert tests == ["a/b/c"]
