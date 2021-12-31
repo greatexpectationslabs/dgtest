7bfc38ff7d8953fcb8ddf0be7d8ffa02dd7a82f7
Chetan Kini <chetan@superconductive.com>
2021-12-31 11:52:54 -0500

    feat: set up 'list' and 'run' cmds

--- dgtest/cli.py
+++ dgtest/cli.py
@@ -1,10 +1,9 @@
-from typing import List, Optional, Tuple
+import sys
+from typing import Callable, List, Optional, Tuple
 
 import click
 
-from dgtest.filesystem import get_changed_files
-from dgtest.graph import determine_tests_to_run
-from dgtest.parse import get_dependency_graphs
+from dgtest.main import determine_test_list, run_tests
 
 
 @click.group()
@@ -12,46 +11,66 @@ def cli() -> None:
     pass
 
 
-@cli.command(help="Run dependency graph test runner")
-@click.option(
-    "-t",
-    "--tests",
-    "tests",
-    help="The relative path to your tests directory (if applicable)",
+shared_options = [
+    click.option(
+        "-t",
+        "--tests",
+        "tests",
+        help="The relative path to your tests directory (if applicable)",
+        type=click.Path(exists=True),
+    ),
+    click.option(
+        "-d",
+        "--depth",
+        "depth",
+        help="The desired maximum depth of the graph traversal algorithm",
+        default=3,
+        type=int,
+    ),
+    click.option(
+        "-i",
+        "--ignore",
+        "ignore_paths",
+        help="Exclude results that begin with any provided path prefix",
+        type=click.Path(),
+        multiple=True,
+    ),
+    click.option(
+        "-f",
+        "--filter",
+        "filter_",
+        help="Filter results to only include those that being with the provided path prefix",
+        type=click.Path(),
+    ),
+    click.option(
+        "-b",
+        "--branch",
+        "branch",
+        help="The specific branch to diff against",
+        type=str,
+    ),
+]
+
+
+def add_options(options: List[Callable]) -> Callable:
+    def _add_options(func: Callable) -> Callable:
+        for option in reversed(options):
+            func = option(func)
+        return func
+
+    return _add_options
+
+
+@cli.command(
+    name="list",
+    help="Print list of test files determined by dependency graph test algorithm",
+)
+@add_options(shared_options)
+@click.argument(
+    "source",
     type=click.Path(exists=True),
 )
-@click.option(
-    "-d",
-    "--depth",
-    "depth",
-    help="The desired maximum depth of the graph traversal algorithm",
-    default=3,
-    type=int,
-)
-@click.option(
-    "-i",
-    "--ignore",
-    "ignore_paths",
-    help="Exclude results that begin with any provided path prefix",
-    type=click.Path(),
-    multiple=True,
-)
-@click.option(
-    "-f",
-    "--filter",
-    "filter_",
-    help="Filter results to only include those that being with the provided path prefix",
-    type=click.Path(),
-)
-@click.option(
-    "-b",
-    "--branch",
-    "branch",
-    help="The specific branch to diff against",
-    type=str,
-)
-@click.argument("source", type=click.Path(exists=True))
-def run(
+def list_command(
     source: str,
     tests: Optional[str],
     depth: int,
@@ -67,40 +86,27 @@ def run(
         click.echo(file)
 
 
-def determine_test_list(
+@cli.command(
+    name="run",
+    help="Use pytest to run tests determined by dependency graph test algorithm",
+    context_settings=dict(ignore_unknown_options=True),
+)
+@add_options(shared_options)
+@click.argument(
+    "source",
+    type=click.Path(exists=True),
+)
+@click.argument("pytest_args", nargs=-1, type=click.UNPROCESSED)
+def run_command(
     source: str,
     tests: Optional[str],
     depth: int,
-    ignore_paths: List[str],
+    ignore_paths: Tuple[str],
     filter_: Optional[str],
     branch: Optional[str],
-) -> List[str]:
-    """Command used to run dependency graph test strategy on a target codebase.
-
-    Args:
-        source: The relative path to your source directory
-        tests: The relative path to your tests directory
-        depth: The depth of the graph traversal (the larger the number, the greater the coverage but the smaller the specificity)
-        ignore_paths: Any test files that starts with any paths in this collection are ignored in the output
-        filter_: Only test paths that start with this value are included in the output
-        branch: The specific branch to diff against to determine changed files
-
-    Returns:
-        A list of test files to run
-
-    """
-    changed_source_files, changed_test_files = get_changed_files(branch)
-    source_dependency_graph, tests_dependency_graph = get_dependency_graphs(
-        source, tests
+    pytest_args: Tuple[str],
+) -> None:
+    exit_code = run_tests(
+        source, tests, depth, list(ignore_paths), filter_, branch, list(pytest_args)
     )
-    files_to_test = determine_tests_to_run(
-        changed_source_files,
-        changed_test_files,
-        source_dependency_graph,
-        tests_dependency_graph,
-        depth,
-        ignore_paths,
-        filter_,
-    )
-
-    return files_to_test
+    sys.exit(exit_code)
